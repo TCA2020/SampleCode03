a3dc352239f1e1bd34c920b84ae004c9c7efc646
ryouhei-kanezawa <k.ryouhei1414@gmail.com>
2020-10-13 17:13:21 +0900

    プレイヤーの名前を入力できるように

--- ConsoleQuest/Program.cs
+++ ConsoleQuest/Program.cs
@@ -10,9 +10,12 @@ namespace ConsoleQuest
 
 			Logger.Log("Start Game!");
 
+			Console.WriteLine("プレイヤーの名前を入力してください");
+
+			string Playname = Console.ReadLine();
 
 			//create player
-			Player player = new Player("プレイヤー", 100f, 10f, 5f, 1, 0);
+			Player player = new Player(Playname, 100f, 10f, 5f, 1, 0);
 
 			//create world
 			World world = new World(player);
