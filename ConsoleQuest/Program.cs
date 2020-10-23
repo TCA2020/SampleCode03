fe0f44e4504cf67744fca26e1c3924e3d435ddf9
fuka-show <68270717+fuka-show@users.noreply.github.com>
2020-10-23 14:32:36 +0900

    json作成

--- ConsoleQuest/Program.cs
+++ ConsoleQuest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace ConsoleQuest
 {
@@ -10,9 +11,33 @@ namespace ConsoleQuest
 
 			Logger.Log("Start Game!");
 
+			Logger.Log("プレイヤーの名前を入力してください");
+
+			string Playname = Logger.ReadInput();
+
+			string currentDerectory=System.IO.Directory.GetCurrentDirectory();
+			//currentDirectonryにjsonが置いてある前提
+			string jsonPath=currentDerectory+"\\Player.txt";
+
+			if ( !System.IO.File.Exists( jsonPath ) )
+			{
+				Player dummy=MakeDefaultData();
+				SavePlayerJson(dummy,jsonPath);
+			}
+
+
+			//	保存されているjsonデータをロードする
+			Player loadedPlayer;
+			if(!LoadPlayerJson(jsonPath, out loadedPlayer ) )
+			{
+				//ロードに失敗した
+				return;
+			}
+
+
 
 			//create player
-			Player player = new Player("プレイヤー", 100f, 10f, 5f, 1, 0);
+			Player player = new Player(Playname, 100f, 10f, 5f, 1, 0);
 
 			//create world
 			World world = new World(player);
@@ -26,6 +51,37 @@ namespace ConsoleQuest
 
 
 			Logger.Log("game over.");
+		}
+
+		private static void SavePlayerJson(Player date,string filePath )
+		{
+			string charajson=Newtonsoft.Json.JsonConvert.SerializeObject(date);
+			System.IO.File.WriteAllText(filePath,charajson);
+		}
+
+		private static bool LoadPlayerJson(string filePath,out Player loadedInstance)
+		{
+			try
+			{
+				string Charajson=System.IO.File.ReadAllText(filePath);
+				loadedInstance=Newtonsoft.Json.JsonConvert.DeserializeObject<Player>(Charajson);
+				return true;
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("ロード失敗:"+e.Message);
+				loadedInstance=null;
+				return false;
+			}
+		}
+
+
+		//Json出力のため、最初に入れておくためのデータを作る
+		private static Player MakeDefaultData( )
+		{
+			Player player=new Player("プレイヤー", 100f, 10f, 5f, 1, 0);
+
+			return player;
 		}
 	}
 }
