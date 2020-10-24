a20c240cb126220e6b9e50ef2e5977b9d5d3fda9
Albinakun <syota_mobile@yahoo.ne.jp>
2020-10-24 15:21:15 +0900

    ver 1.0

--- ConsoleQuest/Battle.cs
+++ ConsoleQuest/Battle.cs
@@ -4,50 +4,54 @@ using System.Text;
 
 namespace ConsoleQuest
 {
-	public class Battle
-	{
-		private Player BattlePlayer;
-		private Enemy BattleEnemy;
+    public class Battle
+    {
+        private Player BattlePlayer;
+        private Enemy BattleEnemy;
 
-		public Battle(Player player, Enemy enemy)
-		{
-			BattlePlayer = player;
-			BattleEnemy = enemy;
-		}
+        public Battle(Player player,Enemy enemy)
+        {
+            BattlePlayer = player;
+            BattleEnemy = enemy;
+        }
 
-		/// <summary>
-		/// ターンを進める
-		/// </summary>
-		/// <returns>処理終了後のバトル状況</returns>
-		public BattleState AdvanceTurn()
-		{
-			//プレイヤーと敵が殴り合う
-			float damage = BattlePlayer.Attack(BattleEnemy);
-			Logger.Log(BattlePlayer.Name + "の攻撃:" + BattleEnemy.Name + "に" + damage + "のダメージ");
-			
-			if(!BattleEnemy.IsAlive)
-			{
-				Logger.Log(BattleEnemy.Name + "を倒した！");
-				return BattleState.Win;
-			}
+        /*<summary>*/
+        /*ターンを進める*/
+        /*</summary>*/
+        /*<returns>処理終了後のバトル状況</returns>*/
+        public BattleState AdvanceTurn()
+        {
+            /*プレイヤーと敵が殴り合う*/
+            float damage = BattlePlayer.Attack(BattleEnemy);
+            Logger.Log(BattlePlayer.Name + "の攻撃:" + BattleEnemy.Name + "に" + damage + "のダメージ");
+            
 
-			damage = BattleEnemy.Attack(BattlePlayer);
-			Logger.Log(BattleEnemy.Name + "の攻撃:" + BattlePlayer.Name + "に" + damage + "のダメージ");
 
-			if(!BattlePlayer.IsAlive)
-			{
-				Logger.Log(BattlePlayer.Name + "は倒れた...");
-				return BattleState.Lose;
-			}
+            if(!BattleEnemy.IsAlive)
+            {
+                Logger.Log(BattleEnemy.Name + "を倒した！");
+                return BattleState.Win;
+            }
 
-			return BattleState.Continue;
-		}
-	}
+            damage = BattleEnemy.Attack(BattlePlayer);
+            Logger.Log(BattleEnemy.Name + "の攻撃" + BattlePlayer.Name + "に" + damage + "のダメージ");
+            Logger.Log("敵のHPは" + BattleEnemy.HP + "/" + BattleEnemy.MaxHP);
+            Logger.Log(BattlePlayer.Name + "のHPは"+BattlePlayer.HP+"/"+BattlePlayer.MaxHP);
 
-	public enum BattleState
-	{
-		Win,
-		Lose,
-		Continue,
-	}
+            if(!BattlePlayer.IsAlive)
+            {
+                Logger.Log(BattlePlayer.Name + "は倒れた…");
+                return BattleState.Lose;
+            }
+
+            return BattleState.Continue;
+        }
+    }
+
+    public enum BattleState
+    {
+        Win,
+        Lose,
+        Continue,
+    }
 }
--- ConsoleQuest/Program.cs
+++ ConsoleQuest/Program.cs
@@ -1,34 +1,97 @@
 ﻿using System;
 
+
 namespace ConsoleQuest
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Logger.Inject(new ConsoleLogger(), new ConsoleInput());
 
-			Logger.Log("Start Game!");
+    class program
+    {
 
-			Logger.Log("プレイヤーの名前を入力してください");
+        static void Main(string[] args)
+        {
 
-			string Playname = Logger.ReadInput();
+            string currentDirectory=System.IO.Directory.GetCurrentDirectory();
 
-			//create player
-			Player player = new Player(Playname, 100f, 10f, 5f, 1, 0);
+            string jsonPath=currentDirectory+"\\player.json";
 
-			//create world
-			World world = new World(player);
+            if (!System.IO.File.Exists(jsonPath))
+            {
+                Console.WriteLine("Jsonファイルがないので、ダミーデータを基にJsonを生成して書き込みます…");
+                Console.WriteLine("出力先:"+jsonPath);
 
-			//worldが終了判定(false)を返すまでループ
-			while(world.Loop())
-			{
-				//Enter入力を待つ
-				Logger.ReadInput();
-			}
+                Player dummy=MakeDefaultData();
+                SavePlayerJson(dummy,jsonPath);
+            }
 
-			//THE END
-			Logger.Log("game over.");
-		}
-	}
+            /*保存されているjsonデータをロードする*/
+            Player loadedPlayer;
+            if(!LoadPlayerJson(jsonPath,out loadedPlayer))
+            {
+                /*ロードに失敗した*/
+                return;
+            }
+
+            Logger.Inject(new ConsoleLogger(), new ConsoleInput());
+
+            Logger.Log("Start Game!");
+
+            Logger.Log("プレイヤーの名前を入力してください:");
+
+            string PlayerName = Logger.ReadInput();
+
+            Console.WriteLine("プレイヤー(あなた)の最初のHPは" + loadedPlayer.MaxHP + "です。");
+
+            /*create player*/
+            Player player=new Player(PlayerName, 100f, 10f, 5f, 1, 0);
+
+            /*create world*/
+            World world = new World(player);
+
+            /*worldが終了判定(false)を返すまでループ*/
+            while(world.Loop())
+            {
+                /*Enter入力を待つ*/
+                Logger.ReadInput();
+            }
+        
+            
+
+            /*THE END*/
+            Logger.Log("Game Over.");
+        
+
+        
+    }
+
+        private static void SavePlayerJson(Player data, string filePath)
+        {
+            string jsonText = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            System.IO.File.WriteAllText(filePath, jsonText);
+        }
+
+        private static bool LoadPlayerJson(string filePath,out Player loadedInstance)
+        {
+
+            try
+            {
+                string jsonText=System.IO.File.ReadAllText(filePath);
+                loadedInstance=Newtonsoft.Json.JsonConvert.DeserializeObject<Player>(jsonText);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ロード失敗:"+e.Message);
+                loadedInstance=null;
+                return false;
+            }
+
+        }
+
+        private static Player MakeDefaultData()
+        {
+            Player player=new Player("PlayerName", 100f, 10f, 5f, 1, 0);
+
+            return player;
+        }
+    }
 }
