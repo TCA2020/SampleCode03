2dbbe5ec58c37f369d5a63f609246e818b212e77
Enrico Gunawan <20J70008eg@edu.tca.ac.jp>
2020-10-25 12:45:40 +0900

    aa

--- ConsoleQuest/Battle.cs
+++ ConsoleQuest/Battle.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConsoleQuest
-{
-	public class Battle
-	{
+namespace ConsoleQuest {
+	public class Battle {
 		private Player BattlePlayer;
 		private Enemy BattleEnemy;
 
@@ -22,19 +21,39 @@ namespace ConsoleQuest
 		public BattleState AdvanceTurn()
 		{
 			//プレイヤーと敵が殴り合う
+
+			//追加
+			string playerData = JsonConvert.SerializeObject(BattlePlayer);
+			//追加
+			string enemyData = JsonConvert.SerializeObject(BattleEnemy);
+
 			float damage = BattlePlayer.Attack(BattleEnemy);
+
 			Logger.Log(BattlePlayer.Name + "の攻撃:" + BattleEnemy.Name + "に" + damage + "のダメージ");
 			
-			if(!BattleEnemy.IsAlive)
+			//追加
+			Console.WriteLine("敵のHP : " + BattleEnemy.HP);
+
+
+			if (!BattleEnemy.IsAlive)
 			{
 				Logger.Log(BattleEnemy.Name + "を倒した！");
+				
+
+				//追加
+				Console.WriteLine(playerData);
 				return BattleState.Win;
 			}
 
 			damage = BattleEnemy.Attack(BattlePlayer);
-			Logger.Log(BattleEnemy.Name + "の攻撃:" + BattlePlayer.Name + "に" + damage + "のダメージ");
+			Logger.Log(BattleEnemy.Name + "の攻撃:" + BattlePlayer.Name + "に" + damage + "のダメージ");			
 
-			if(!BattlePlayer.IsAlive)
+			//追加
+			Console.WriteLine("君のHP : " + BattlePlayer.HP);
+
+
+
+			if (!BattlePlayer.IsAlive)
 			{
 				Logger.Log(BattlePlayer.Name + "は倒れた...");
 				return BattleState.Lose;
@@ -44,8 +63,7 @@ namespace ConsoleQuest
 		}
 	}
 
-	public enum BattleState
-	{
+	public enum BattleState {
 		Win,
 		Lose,
 		Continue,
