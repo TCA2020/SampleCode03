13edda58f9c4f26268b91f664bc0dfeeed321ae5
K-0865 <66938347+K-0865@users.noreply.github.com>
2020-10-28 02:01:59 +0900

    Json Save

---
+++ ConsoleQuest/json.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleQuest
+{
+    public static class json
+	{
+
+
+		
+		public static void savejson(Player player, string savepath)
+		{
+
+			string jsonfile = Newtonsoft.Json.JsonConvert.SerializeObject(player);
+			System.IO.File.WriteAllText(savepath, jsonfile);
+
+		}
+
+			public static bool loaddata(string loadpath , out Player player) {
+
+			try
+			{
+				string jsonf = System.IO.File.ReadAllText(loadpath);
+				player = Newtonsoft.Json.JsonConvert.DeserializeObject<Player> ( jsonf );
+				return true;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("JsonFile not Found");
+				player = null;
+				return false;
+			
+			}
+		
+		}
+	}
+}
