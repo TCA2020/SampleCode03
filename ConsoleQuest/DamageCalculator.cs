f764db883607bd43b8d8c008ec6dafb195c0e8e8
MTDmatsuda <66938540+MTDmatsuda@users.noreply.github.com>
2020-11-15 18:55:00 +0900

    Update DamageCalculator.cs

--- ConsoleQuest/DamageCalculator.cs
+++ ConsoleQuest/DamageCalculator.cs
@@ -17,15 +17,29 @@ namespace ConsoleQuest
 		/// <returns>ダメージ値</returns>
 		public static float CalculateDamage(Character attacker, Character target)
 		{
+			Random rand = new Random();
+			int chance = rand.Next(1, 101);
+
+			 
+			
 			// ドラクエの計算式にならう
 			//（攻撃力/2-守備力/4）×乱数(7/8~9/8)
 			float damage = (attacker.AttackPoint / 2 - target.DefencePoint / 4) * (0.875f + (float)RandomGenerator.NextDouble() * 0.25f);
-			//小数点切り捨て
-			damage = (float)Math.Floor(damage);
+			float damageee = (attacker.AttackPoint * 2);
+			if (chance <= 10)
+            {
+				Logger.Log("会心の一撃");
+				damage = damageee;
+            }
+				damage = (float)Math.Floor(damage);
+			
 			//最低ダメージ保証
 			if (damage < 1f) damage = 1f;
+			
 
 			return damage;
 		}
+		
 	}
+
 }
